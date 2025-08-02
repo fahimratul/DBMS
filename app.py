5b3190fc9e0b923b488527f09d98ded76dac94fb
RATUL <fmratul5@gmail.com>
2025-08-02 15:34:49 +0600

    flask update in signup btn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template , url_for , request
 
 app = Flask(__name__)
 
@@ -10,6 +10,14 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    role = request.form.get('role')
+    if role:
+        return render_template('signupform.html', role=role)
+    else:
+        return render_template('login.html', error="Please select a role to continue.")
 
 if __name__ == '__main__':
     app.run(debug=True)
