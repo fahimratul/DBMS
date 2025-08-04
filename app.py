df38e780afadcf643bf25adecd42d3d8df22d49d
RATUL <fmratul5@gmail.com>
2025-08-05 02:43:17 +0600

    upto today

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask , render_template , url_for , request
 
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -28,5 +30,11 @@ def signup():
 @app.route('/admin')
 def admin():
     return render_template('admin/admin.html')
+
+@app.route('/admin/volunteers')
+def volunteer_list():
+    return render_template('admin/volunteer_list.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
