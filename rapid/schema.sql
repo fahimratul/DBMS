50e288255675824c9f6cc00e31bfd91d94da0ea8
RATUL <fmratul5@gmail.com>
2025-08-30 20:12:43 +0600

    update schema in oder list

--- rapid/schema.sql
+++ rapid/schema.sql
@@ -8,17 +8,6 @@ CREATE TABLE donor (
     account_id INT NOT NULL,
     address TEXT
 );
-
-CREATE TABLE donation (
-    donation_id INT PRIMARY KEY AUTO_INCREMENT,
-    message TEXT,
-    date DATE,
-    donor_id INT,
-    item_id INT,
-    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
-    FOREIGN KEY (item_id) REFERENCES item(item_id)
-);
-
 CREATE TABLE receiver (
     receiver_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
@@ -27,37 +16,6 @@ CREATE TABLE receiver (
     password TEXT NOT NULL,
     emergency_phone VARCHAR(20),
     address TEXT
-);
-
-CREATE TABLE donation_receiver (
-    donation_receiver_id INT PRIMARY KEY AUTO_INCREMENT,
-    receiver_id INT,
-    date DATE NOT NULL ,
-    priority_message TEXT,
-    stock_id INT,
-    additional_item TEXT,
-    FOREIGN KEY (stock_id) REFERENCES stock(stock_id),
-    FOREIGN KEY (receiver_id) REFERENCES receiver(receiver_id)
-);
-
-CREATE TABLE stock (
-    stock_id INT PRIMARY KEY AUTO_INCREMENT,
-    price INT NOT NULL CHECK (price >= 0),
-    quantity INT CHECK (quantity >= 0),
-    purchase_date DATE NOT NULL ,
-    stock_date DATE NOT NULL ,
-    expire_date DATE NOT NULL ,
-    item_id INT,
-    account_id INT,
-    FOREIGN KEY (item_id) REFERENCES item(item_id),
-    FOREIGN KEY (account_id) REFERENCES account(account_id)
-);
-
-CREATE TABLE item (
-    item_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50),
-    type_id INT,
-    FOREIGN KEY (type_id) REFERENCES type_list(type_id)
 );
 
 CREATE TABLE type_list (
@@ -91,19 +49,47 @@ CREATE TABLE volunteer (
     nid_birthcert BLOB
 );
 
-CREATE TABLE feedback (
-    feedback_id INT PRIMARY KEY AUTO_INCREMENT,
-    receiver_id INT,
-    volunteer_id INT,
-    donor_id INT,
-    task_id INT,
-    message TEXT,
-    picture BLOB,
-    FOREIGN KEY (receiver_id) REFERENCES receiver(receiver_id),
-    FOREIGN KEY (volunteer_id) REFERENCES volunteer(volunteer_id),
-    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
-    FOREIGN KEY (task_id) REFERENCES event(task_id)
+CREATE TABLE item (
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    type_id INT,
+    FOREIGN KEY (type_id) REFERENCES type_list(type_id)
 );
+
+CREATE TABLE donation (
+    donation_id INT PRIMARY KEY AUTO_INCREMENT,
+    message TEXT,
+    date DATE,
+    donor_id INT,
+    item_id INT,
+    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
+    FOREIGN KEY (item_id) REFERENCES item(item_id)
+);
+
+CREATE TABLE stock (
+    stock_id INT PRIMARY KEY AUTO_INCREMENT,
+    price INT NOT NULL CHECK (price >= 0),
+    quantity INT CHECK (quantity >= 0),
+    purchase_date DATE NOT NULL ,
+    stock_date DATE NOT NULL ,
+    expire_date DATE NOT NULL ,
+    item_id INT,
+    account_id INT,
+    FOREIGN KEY (item_id) REFERENCES item(item_id),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+CREATE TABLE donation_receiver (
+    donation_receiver_id INT PRIMARY KEY AUTO_INCREMENT,
+    receiver_id INT,
+    date DATE NOT NULL ,
+    priority_message TEXT,
+    stock_id INT,
+    additional_item TEXT,
+    FOREIGN KEY (stock_id) REFERENCES stock(stock_id),
+    FOREIGN KEY (receiver_id) REFERENCES receiver(receiver_id)
+);
+
 
 CREATE TABLE event (
     task_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -125,3 +111,17 @@ CREATE TABLE money_transfer (
     FOREIGN KEY (account_id) REFERENCES account(account_id),
     FOREIGN KEY (donation_id) REFERENCES donation(donation_id)
 )
+
+CREATE TABLE feedback (
+    feedback_id INT PRIMARY KEY AUTO_INCREMENT,
+    receiver_id INT,
+    volunteer_id INT,
+    donor_id INT,
+    task_id INT,
+    message TEXT,
+    picture BLOB,
+    FOREIGN KEY (receiver_id) REFERENCES receiver(receiver_id),
+    FOREIGN KEY (volunteer_id) REFERENCES volunteer(volunteer_id),
+    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
+    FOREIGN KEY (task_id) REFERENCES event(task_id)
+);
